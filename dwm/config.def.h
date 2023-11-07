d07a0cb261bea54c95cafb2f2c9673da9d37c935
aquaverso2077 <aquaverso77@tutanota.com>
2023-11-07 18:47:27 +0000

    dwm gaps

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -3,7 +3,6 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const unsigned int gappx     = 4;
 static const char *fonts[]          = { "Iosevka Nerd Font Mono:size=12" };
 /* color */
 static const char col_gray1[]       = "#1d2021";
@@ -109,6 +108,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
