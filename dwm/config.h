7e3d2292af1786a32ae937ac4f077334f8fad4b1
aquaverso2077 <aquaverso77@tutanota.com>
2023-11-16 13:46:35 +0000

    fix

--- dwm/config.h
+++ dwm/config.h
@@ -2,9 +2,9 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const unsigned int gappx     = 4;
-static const char *fonts[]          = { "Iosevka Nerd Font Mono:size=12" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
 /* color */
 static const char col_gray1[]       = "#1d2021";
 static const char col_gray2[]       = "#B16286";
